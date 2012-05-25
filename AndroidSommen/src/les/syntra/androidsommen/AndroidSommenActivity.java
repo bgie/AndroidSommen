2555c81bdb9809ffa7a2a7a2778cfab4490e1915
bgie <brecht.kuppens@gmail.com>
2012-05-25 21:52:42 +0200

    Eerste commit leeg project Android 2.1

---
+++ AndroidSommen/src/les/syntra/androidsommen/AndroidSommenActivity.java
@@ -0,0 +1,13 @@
+package les.syntra.androidsommen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidSommenActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
