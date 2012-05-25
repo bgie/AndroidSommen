48c0385a7eddc7f79c0f6853a4f140ac853ed8fd
habra <habra@dellhabra.ucad.local>
2012-05-25 22:21:36 +0200

    hoi pipeloi

--- AndroidSommen/src/les/syntra/androidsommen/AndroidSommenActivity.java
+++ AndroidSommen/src/les/syntra/androidsommen/AndroidSommenActivity.java
@@ -2,9 +2,13 @@ package les.syntra.androidsommen;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AndroidSommenActivity extends Activity {
     /** Called when the activity is first created. */
+
+    
+    private TextView txtTest;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
