cdd0c25e1840fcb3e5ab2071e5cee71b63d83db9
bgie <brecht.kuppens@gmail.com>
2012-05-29 19:57:56 +0200

    Gitignore aangepast, binaire en gegenereerde bestanden uit git gehaald.
    + opkuisen code en verwijderen zever in commentaren.

--- AndroidSommen/src/les/syntra/androidsommen/AndroidSommenActivity.java
+++ AndroidSommen/src/les/syntra/androidsommen/AndroidSommenActivity.java
@@ -7,17 +7,9 @@ import android.widget.TextView;
 public class AndroidSommenActivity extends Activity {
     /** Called when the activity is first created. */
 
-    // We zitten nu in een parallel universum!
-	
-    private TextView txtTest;
-    //Brecht Jr. Reporting in: dit is van desktop!
-    // Brecht was HIER!
-    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        // Nog 
     }
 }
