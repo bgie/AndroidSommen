03764bc05a8d84206ad881afd76dcb41cdfcead6
bgie <brecht.kuppens@gmail.com>
2012-06-01 22:11:10 +0200

    quote foutje

--- AndroidSommen/gen/les/syntra/androidsommen/R.java
+++ AndroidSommen/gen/les/syntra/androidsommen/R.java
@@ -14,16 +14,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int btnChoosePlayer=0x7f050007;
+        public static final int btnChoosePlayer=0x7f05000d;
         public static final int btnCreatePlayer=0x7f050006;
-        public static final int btnHighScores=0x7f050009;
-        public static final int btnSettings=0x7f050008;
-        public static final int btnStart=0x7f05000a;
+        public static final int btnHighScores=0x7f05000f;
+        public static final int btnSettings=0x7f05000e;
+        public static final int btnStart=0x7f050010;
+        public static final int btnSubmit=0x7f05000c;
+        public static final int lblLevel=0x7f050007;
+        public static final int lblQuestion=0x7f05000a;
+        public static final int lblScore=0x7f050009;
+        public static final int lblTime=0x7f050008;
         public static final int listView1=0x7f050000;
         public static final int textView=0x7f050001;
         public static final int textView2=0x7f050002;
         public static final int textView3=0x7f050004;
         public static final int txtAge=0x7f050005;
+        public static final int txtInput=0x7f05000b;
         public static final int txtName=0x7f050003;
     }
     public static final class layout {
@@ -36,5 +42,10 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int txtBtnSubmit=0x7f040005;
+        public static final int txtLblLevel=0x7f040003;
+        public static final int txtLblScore=0x7f040002;
+        public static final int txtLblTime=0x7f040004;
+        public static final int txtlblQuestion=0x7f040006;
     }
 }
