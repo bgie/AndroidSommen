35ace34540b0aedda8ecf52476492faa2b23ae43
boduiza <123riktik5>
2012-06-01 22:00:04 +0200

    Startscherm+chooseplayer scherm

--- AndroidSommen/gen/les/syntra/androidsommen/R.java
+++ AndroidSommen/gen/les/syntra/androidsommen/R.java
@@ -14,13 +14,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int textView1=0x7f050000;
+        public static final int btnChoosePlayer=0x7f050007;
+        public static final int btnCreatePlayer=0x7f050006;
+        public static final int btnHighScores=0x7f050009;
+        public static final int btnSettings=0x7f050008;
+        public static final int btnStart=0x7f05000a;
+        public static final int listView1=0x7f050000;
+        public static final int textView=0x7f050001;
+        public static final int textView2=0x7f050002;
+        public static final int textView3=0x7f050004;
+        public static final int txtAge=0x7f050005;
+        public static final int txtName=0x7f050003;
     }
     public static final class layout {
-        public static final int game=0x7f030000;
-        public static final int highscores=0x7f030001;
-        public static final int settings=0x7f030002;
-        public static final int start=0x7f030003;
+        public static final int chooseplayer=0x7f030000;
+        public static final int game=0x7f030001;
+        public static final int highscores=0x7f030002;
+        public static final int settings=0x7f030003;
+        public static final int start=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
