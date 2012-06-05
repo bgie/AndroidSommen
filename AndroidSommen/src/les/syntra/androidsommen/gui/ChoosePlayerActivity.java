6e41498ac8b324ad539435f162fd96320ef498c6
boduiza <123riktik5>
2012-06-05 22:16:36 +0200

    chooseplayer activity aangepast

--- AndroidSommen/src/les/syntra/androidsommen/gui/ChoosePlayerActivity.java
+++ AndroidSommen/src/les/syntra/androidsommen/gui/ChoosePlayerActivity.java
@@ -1,10 +1,22 @@
 package les.syntra.androidsommen.gui;
 
+import java.io.IOException;
+
+import org.json.JSONException;
+
 import les.syntra.androidsommen.R;
+import les.syntra.androidsommen.logic.Database;
+import les.syntra.androidsommen.logic.Player;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ChoosePlayerActivity extends Activity{
+	EditText txtName, txtAge;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,6 +24,48 @@ public class ChoosePlayerActivity extends Activity{
         String appName = getString(R.string.app_name);
         String activeScreen = getString(R.string.txtBtnChoosePlayer);
         setTitle(appName + " -> " + activeScreen);
+        Button btnCreatePlayer=(Button)findViewById(R.id.btnCreatePlayer);
+        
+        try {
+        	try {
+				btnCreatePlayer.setOnClickListener(new CreateNewPlayer());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        } catch(JSONException e) {
+        	e.printStackTrace();
+        }
 	}
 	
+	class CreateNewPlayer  implements OnClickListener {
+		@Override
+		public void onClick(View v){
+			
+			String strName=""+ txtName.getText();
+			Integer intAge=Integer.parseInt( txtAge.getText().toString());
+			
+			Player plNew = new Player(strName,intAge) ;
+			/*			try {
+				plNew.toJSON();
+				try {
+				saveAll();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		*/	
+			
+		}
+		
+		public CreateNewPlayer() throws JSONException, IOException{
+			
+		}
+		
+	}
 }
